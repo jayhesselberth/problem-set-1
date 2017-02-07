a1e22958f0cbc682e132c016c6a7f6110c45f50f
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-02-07 14:46:31 -0700

    just gave everybody 2 points

--- answers.sh
+++ answers.sh
@@ -5,13 +5,20 @@ datasets='/Users/jayhesselberth/devel/MOLB7621/data-sets'
 
 states="$datasets/misc/states.tab.gz"
 
+# Which state in states.tab.gz has the lowest murder rate?
+
 answer_1=$(gzcat $states \
     | grep -v '^#' \
-    | cut -f1,2 \
-    | sort -k2n \
-    | tail -n1 \
-    | cut -f1 \
-    | sed 's/"//g') 
+    | cut -f1,6 \
+    | sort -k2g \
+    | head -n1 \
+    | cut -f1)
 
 echo "answer-1: $answer_1"
+
+# How many sequence records are in the sample.fa file?
+
+sample="$datasets/fasta/sample.fa"
+answer_2=$(grep '^>' $sample | wc -l)
+echo "answer-2: $answer_2"
 
